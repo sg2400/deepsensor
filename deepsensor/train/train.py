4db49348bf7a34d3c85e7394206643ffd632a1f1
Martin Rogers <marrog@sci-vm-02.jasmin.ac.uk>
2025-06-02 21:10:56 +0100

    small edit to address optimiser errors

--- deepsensor/train/train.py
+++ deepsensor/train/train.py
@@ -149,20 +149,6 @@ def train_epoch(
             return mean_batch_loss.detach().cpu().numpy()  # Return the loss value (detached)
 
         
-        """
-        def train_step(tasks):
-            if not isinstance(tasks, list):
-                tasks = [tasks]
-            opt.zero_grad()
-            task_losses = []
-            for task in tasks:
-                task_losses.append(model.loss_fn(task, normalise=True))
-            mean_batch_loss = B.mean(B.stack(*task_losses))
-            mean_batch_loss.backward()
-            opt.step()
-            return mean_batch_loss.detach().cpu().numpy()
-        """
-
     else:
         raise NotImplementedError(f"Backend {deepsensor.backend.str} not implemented")
 
@@ -186,7 +172,7 @@ def train_epoch(
             )
         else:
             task = tasks[batch_i]
-        batch_loss = train_step(task, opt, scaler)
+        batch_loss = train_step(task, scaler)
         batch_losses.append(batch_loss)
 
     return batch_losses
@@ -200,7 +186,6 @@ class Trainer:
     """
 
     def __init__(self, model: ConvNP, lr: float = 5e-5):
-        print('running edittable model train model')
         if deepsensor.backend.str == "tf":
             import tensorflow as tf
 
@@ -226,7 +211,7 @@ class Trainer:
             tasks=tasks,
             batch_size=batch_size,
             opt=self.opt,
-            scaler=scaler,  # Pass scaler into train_epoch
+            scaler=scaler,
             progress_bar=progress_bar,
             tqdm_notebook=tqdm_notebook,
         )
